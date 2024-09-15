7d67f5076907cdec3a1fdeb1a46dd239aba641be
Overkill26 <jover26@outlook.com>
2024-09-16 09:03:58 +1000

    Initialisation and Testing

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+package_name = 'egh400_hmi_robotic_arm'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Your Name',
+    maintainer_email='your_email@example.com',
+    description='A ROS 2 package for EGH400 HMI Robotic Arm',
+    license='Apache 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'simple_publisher = egh400_hmi_robotic_arm.simple_publisher:main',
+        ],
+    },
+)
